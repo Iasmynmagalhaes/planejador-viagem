756790a6e2599826852872848026c1bd1c8ca3b3
Iasmynmagalhaes <ias.mag.fer@gmail.com>
2026-05-03 17:01:49 -0300

    removendo conflitos restantes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-=======
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
->>>>>>> c31c3b27c2dc6c42b285efdd9159ae7e9c49206f
+  plugins: []
 }
